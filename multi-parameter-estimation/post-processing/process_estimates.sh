de5145a3d97c25b5ac150589dcaa42c14b8fbd10
Joseph Niblo <61010077+josephniblo@users.noreply.github.com>
2025-06-03 16:28:48 +0100

    Add and analyze using raw tags

--- multi-parameter-estimation/post-processing/process_estimates.sh
+++ multi-parameter-estimation/post-processing/process_estimates.sh
@@ -4,8 +4,6 @@
 set -e
 
 pipenv run papermill ./post-processing/1.0_label_coincidences.ipynb /dev/null
-pipenv run papermill ./post-processing/1.1_sum_and_scale_coincidences.ipynb /dev/null
-pipenv run papermill ./post-processing/2_chunk_to_forties.ipynb /dev/null
-pipenv run papermill ./post-processing/3_create_higher_n_estimates.ipynb /dev/null
+pipenv run papermill ./post-processing/1.1_sum_scale_and_chunk.ipynb /dev/null
 pipenv run papermill ./post-processing/4_estimate_on_chunks.ipynb /dev/null
 pipenv run papermill ./post-processing/5_analyse_estimates.ipynb /dev/null
--- multi-parameter-estimation/scripts/prepare_and_estimate_states.sh
+++ multi-parameter-estimation/scripts/prepare_and_estimate_states.sh
@@ -3,9 +3,10 @@
 # Abort on error
 set -e
 
-pipenv run python ./scripts/prepare_and_estimate_states.py
+pipenv run python ./scripts/prepare_and_estimate_states_with_tomtag.py
 
-pipenv run papermill ./post-processing/1.0_label_coincidences.ipynb /dev/null
+pipenv run papermill ./post-processing/1.0.0_sort_tags.ipynb /dev/null
+pipenv run papermill ./post-processing/1.0.1_label_coincidences.ipynb /dev/null
 pipenv run papermill ./post-processing/1.1_sum_and_scale_coincidences.ipynb /dev/null
 pipenv run papermill ./post-processing/2_chunk_to_forties.ipynb /dev/null
 pipenv run papermill ./post-processing/3_create_higher_n_estimates.ipynb /dev/null
